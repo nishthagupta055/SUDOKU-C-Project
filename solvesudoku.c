58129bf22fb92c84fdd4ff70af52a66eab3bafa6
Nishtha Gupta <70621712+nishthagupta055@users.noreply.github.com>
2021-09-12 01:32:14 +0530

    Update solvesudoku.c

--- solvesudoku.c
+++ solvesudoku.c
@@ -205,21 +205,27 @@ bool validate()
 // function to print the sudoku 
 void print()
 {
-    int i,j;
-    printf("-------------------------------------\n");
-    for(i=0;i<9;i++)
-    {
-        for(j=0;j<9;j++)
-        {
-            printf("%d   ",arr[i][j]);
-             
-        
+   
+    int i, j,a;
+
+    printf("\n");
+    printf(" 0 | 1  2  3 | 4  5  6 | 7  8  9 | \n");
+    printf(" ---------------------------------\n");
+    for (i = 0, a = 1; i < 9; i++, a++){
+        for(j = 0; j < 9; j++){
+            if (j == 0)
+                printf(" %d |", a);
+            else if ((j) % 3 == 0)
+                printf("|");
+            printf(" %d ", arr[i][j]);
+            if (j == 8)
+                printf("|");
         }
         printf("\n");
-        printf("-------------------------------------");
-        printf("\n");
-        
+        if ((i + 1) % 3 == 0)
+            printf(" ---------------------------------\n");
     }
+    
 }
 
 // reset to original sudoku of the same level
